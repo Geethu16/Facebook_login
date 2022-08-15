2db31aa1b7e57c04368e2b39a6eb4d7227f1f80d
Geethu Justin <87414068+Geethu16@users.noreply.github.com>
2022-08-15 16:38:48 +0530

    remote add

---
+++ webapp/urls.py
@@ -0,0 +1,45 @@
+"""webapp URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.contrib.auth.views import LoginView, LogoutView
+
+from display.views import (
+    home_view,
+    dashboard_view,
+    settings_view,
+    password_view
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', home_view, name='home'),
+    path('dashboard/', dashboard_view, name='dashboard'),
+]
+
+urlpatterns += [
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+]
+
+urlpatterns += [
+    path('oauth/', include('social_django.urls', namespace='social')),
+]
+
+urlpatterns += [
+    path('settings/', settings_view, name='settings'),
+    path('password/', password_view, name='password'),
+]
